2c75201ccf3998d0bdcf275cc4a441254d16a272
Bach Bui <bui.duybach@gmail.com>
2022-11-03 13:41:08 -0700

    update udf

--- udf_seaborn.py
+++ udf_seaborn.py
@@ -1,40 +1,51 @@
+import inspect
 import seaborn as sns
-from cnextlib.udf import register_udf, Config, View, OutputType, Position, Shape, Location, View, clear_udfs
+from cnextlib.udf_manager import register_udf, Config, View, OutputType, Position, Shape, Location, View, clear_udfs
 import matplotlib.pyplot as plt
 import matplotlib_inline
 matplotlib_inline.backend_inline.set_matplotlib_formats('svg')
+MAX_POINT_COUNT = 1000
 
 clear_udfs()
 
 
-@register_udf(Config("histogram", OutputType.IMAGE,
-                     {Location.TABLE_HEADER: View(Position(1, 0)),
-                      Location.SUMMARY: View(Position(0, 1))}))
+@register_udf(Config("histogram", OutputType.IMAGE, {Location.TABLE_HEADER: View(Position(1, 0)), Location.SUMMARY: View(Position(0, 1))}))
 def histogram(df, col_name):
-    plt.figure(figsize=(8, 4))
-    sns.histplot(df[col_name], color="#3283FE")
-    plt.xlabel("")
-    plt.ylabel("")
-    plt.show()
+    print('Get %s for column %s... ' %
+          (inspect.stack()[0][3], col_name), end='')
+    if df[col_name].dtypes not in ["object"]:
+        plt.figure(figsize=(8, 4))
+        sns.histplot(df[col_name], color="#3283FE")
+        plt.xlabel("")
+        plt.ylabel("")
+        plt.show()
+    else:
+        fig = sns.barplot(df[col_name].value_counts()[:])
+    print('Done')
 
 
-@register_udf(Config("quantile", OutputType.IMAGE,
-                     {Location.SUMMARY: View(Position(0, 0)),
-                      Location.TABLE_HEADER: View(Position(1, 0))}))
+@register_udf(Config("quantile", OutputType.IMAGE, {Location.SUMMARY: View(Position(0, 0)), Location.TABLE_HEADER: View(Position(1, 0))}))
 def quantile(df, col_name):
+    print('Get %s for column %s... ' %
+          (inspect.stack()[0][3], col_name), end='')
     if df[col_name].dtypes not in ["object"]:
         plt.figure(figsize=(10, 2))
         sns.boxplot(data=df, x=col_name, color="#3283FE")
         plt.xlabel("")
         plt.ylabel("")
         plt.show()
+    print('Done')
 
 
 @register_udf(Config("missing values", OutputType.IMAGE,
                      {Location.SUMMARY: View(Position(0, 2), Shape(200, 50))}))
 def missing_value(df, col_name):
+    print('Get %s for column %s... ' %
+          (inspect.stack()[0][3], col_name), end='')
     if str(type(df)) == "<class 'cnextlib.dataframe.DataFrame'>":
         tmp_df = df.df
+    else:
+        tmp_df = df
     if tmp_df[col_name].isna().sum() > 0:
         plt.figure(figsize=(5, 0.5))
         res = sns.heatmap(tmp_df[[col_name]].isna().transpose(), fmt="g",
@@ -44,3 +55,19 @@ def missing_value(df, col_name):
             spine.set_visible(True)
             spine.set_linewidth(0.5)
         plt.show()
+    print('Done')
+
+
+@register_udf(Config("violin_plot", OutputType.IMAGE,
+                     {Location.TABLE_HEADER: View(Position(1, 0)),
+                      Location.SUMMARY: View(Position(0, 1))}))
+def violin_plot(df, col_name):
+    print('Get %s for column %s... ' %
+          (inspect.stack()[0][3], col_name), end='')
+    if df[col_name].dtypes not in ["object"]:
+        ar = df[col_name]
+        sns.violinplot(ar)
+        plt.show()
+    else:
+        return None
+    print('Done')
